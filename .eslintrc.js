6896acf91994d0e490f57f24ef6bdc770a92c0da
usaidmasud <syaripmasud@gmail.com>
2024-01-05 18:32:20 +0800

    format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,37 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   settings: {
     react: {
-      version: 'detect'
-    }
+      version: "detect",
+    },
   },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended'
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
   ],
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
-      files: ['.eslintrc.{js,cjs}'],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script'
-      }
-    }
+        sourceType: "script",
+      },
+    },
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ['@typescript-eslint', 'react'],
+  plugins: ["@typescript-eslint", "react"],
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    'prettier/prettier': 'on'
-  }
+    "react/react-in-jsx-scope": "off",
+  },
 };
--- src/utils/services/instance.ts
+++ src/utils/services/instance.ts
@@ -1,8 +1,8 @@
 // axiosConfig.js
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'https://swapi.dev/api/',
+  baseURL: "https://swapi.dev/api/",
   timeout: 5000,
   // You can add more config options as needed
 });
@@ -11,28 +11,28 @@ const instance = axios.create({
 instance.interceptors.request.use(
   (config) => {
     // You can modify the request config here, e.g., add headers
-    console.log('Request Interceptor:', config);
+    console.log("Request Interceptor:", config);
     return config;
   },
   (error) => {
     // Handle request error
-    console.error('Request Interceptor Error:', error);
+    console.error("Request Interceptor Error:", error);
     return Promise.reject(error);
-  }
+  },
 );
 
 // Response interceptor
 instance.interceptors.response.use(
   (response) => {
     // You can modify the response data here
-    console.log('Response Interceptor:', response);
+    console.log("Response Interceptor:", response);
     return response;
   },
   (error) => {
     // Handle response error
-    console.error('Response Interceptor Error:', error);
+    console.error("Response Interceptor Error:", error);
     return Promise.reject(error);
-  }
+  },
 );
 
 export default instance;
